fc14011f27c5769b24094a479f047f553c55b1ea
JohnJairoRA <dragonnegrojj@gmail.com>
2025-03-03 21:07:19 -0500

    Función para agregar un amigo y limpiar la caja de texto

--- app.js
+++ app.js
@@ -21,3 +21,19 @@ function establecerNumeroDeAmigos(numeroDeAmigos) {
     const amigoInput = document.getElementById('amigo');
     amigoInput.placeholder = `Escribe un nombre (1 de ${numeroDeAmigos})`;
 }
+// Función para agregar un amigo y limpiar la caja de texto
+function agregarAmigo() {
+    const amigoInput = document.getElementById('amigo');
+    const nombreAmigo = amigoInput.value.trim();
+
+    if (Amigos.length >= numeroDeAmigos) {
+        alert('El límite de amigos fue alcanzado, no puedes agregar más amigos.');
+    } else if (!nombreAmigo || !isNaN(nombreAmigo)) {
+        alert('Digita un nombre, el número no es válido.');
+    } else {
+        Amigos.push(nombreAmigo);
+        console.log(`Amigo añadido: ${nombreAmigo}`);
+        actualizarListaAmigos();
+        amigoInput.value = ''; // Limpiar la caja de texto
+    }
+}
